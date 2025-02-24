c29cf6ce5315d7bd40fa1c24d7a1775c162f8249
Thomas K Olsen <thomaskolsen89@gmail.com>
2025-02-24 16:46:48 +0100

    Locval repository as descrived in question 5 commit 3

--- User_input_validator.py
+++ User_input_validator.py
@@ -1,17 +1,15 @@
 class UserInputValidator:
-    def __init__(self):
+    def __init__(self, input_list):  
+        self.input_list = input_list
         self.is_digit = []
-    
-    def validate_positive_integers(self, lst):
-        self.is_digit = [] # empty list
-        for el in lst:
-            if el.isdigit(): # True if el is a positiv integer (including zero)
+        self.validate_positive_integers()  
+
+    def validate_positive_integers(self):
+        self.is_digit = []  
+        for el in self.input_list:
+            if isinstance(el, str) and el.isdigit():  
                 self.is_digit.append(el)
         return self.is_digit
 
-# test data --> ['2', '6', '0'] if correct
-test_list = ["2", "6", "-2", "0", "-100", "Hello", "!"]
-
-uiv = UserInputValidator()
-
-print(uiv.validate_positive_integers(test_list))
+    def __str__(self):  
+        return str(self.is_digit)
--- instances.py
+++ instances.py
@@ -1,5 +1,12 @@
-import User_input_validator
+from User_input_validator import UserInputValidator
+
+# test lists
+ts1 = ["r", 5, "5", "-30", 11, "35"] # --> ["5", "35"]
+ts2 = ["Hello", "10", "11", "-1", 123] # --> ["10", "11"]
 
 # Instances
-inst1 = User_input_validator.UserInputValidator()
-inst2 = User_input_validator.UserInputValidator()
+inst1 = UserInputValidator(ts1)
+inst2 = UserInputValidator(ts2)
+
+print(inst1)
+print(inst2)
